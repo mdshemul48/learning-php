d7bed4eee0990300cc276208867b7cdce0aa0154
MD. Shimul <mdshemul480@gmail.com>
2024-07-22 23:27:26 +0600

    adding special char in html

--- template/jokeList.php
+++ template/jokeList.php
@@ -11,7 +11,7 @@
         <?php foreach ($result as $row) : ?>
             <tr>
                 <td><?= $row["id"] ?></td>
-                <td><?= $row["joketext"] ?></td>
+                <td><?= htmlspecialchars($row["joketext"], ENT_QUOTES, "UTF-8") ?></td>
                 <td><?= $row["jokedate"] ?></td>
                 <td>
                     <form action="deletejoke.php" method="post">
